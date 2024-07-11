a5b221eafc4f936a38119818cf40cdccc79095b6
Mihai <trofimciuc.m@gmail.com>
2024-07-10 22:23:59 -0400

    Created About and Navbar components and styles

---
+++ vite-project/src/components/About.jsx
@@ -0,0 +1,20 @@
+import "../styles/About.css";
+
+export const About = () => {
+	return (
+		<div className="container">
+			<img src="" alt=""/>
+			<h1>About me</h1>
+			<h2>My name is Michael, I'm ReactJS Developer.</h2>
+			<h1>Contact Details</h1>
+			<p>
+				Michael Trofimciuc &nbsp;
+				123 Fake Street &nbsp;
+				Canada CA, ABC 123
+				+1 514-123-45678
+				trofimciuc.m@gmail.com
+			</p>
+
+		</div>
+	);
+}
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "../styles/Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<ul className="nav-items">
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/about">About</Link></li>
+			</ul>
+		</nav>
+	);
+}
