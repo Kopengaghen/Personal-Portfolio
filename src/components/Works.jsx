30aea527f6de14aaca80266e1aa13a9eb1c863e2
Mihai <trofimciuc.m@gmail.com>
2025-03-04 23:13:15 -0500

    Added some css for works.css and added a link for pass checker project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,10 +4,12 @@ export const Works = () => {
 	return (
 		<div>
 			<h2>CHECK OUT SOME OF MY WORKS.</h2>
-			<h2>ToDO List</h2>
-			<ul>
-				<li><a href="https://todomike.netlify.app/" target="todo" rel="">View Project</a></li>
-			</ul>
+			<div className="project">
+				<li><a href="https://todomike.netlify.app/" target="todo" rel="">ToDO List</a></li>
+			</div>
+			<div className="project">
+				<li><a href="https://pass-checker.netlify.app/" target="pass-check" rel="">Password Checker</a></li>
+			</div>
 		</div>
-	)
-}
+	);
+};
